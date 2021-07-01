ffe1f76ec3feee43306d9ad9466e5f0950153226
andows159 <canalandows@gmail.com>
2021-07-01 13:27:36 -0300

    Toggle button responsive animation

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    /* Animation for scroll down page navbar */
     $(window).scroll(function(){
         if(this.scrollY > 25){
             $('.navbar').addClass("sticky")
@@ -7,4 +9,11 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky")
         }
     })
+
+    /* Animation navbar resposive toggle menu */
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+    })
+
+
 });
