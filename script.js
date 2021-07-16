7796699776bb0195348f93b4fef6ff60bdb0e3eb
gerssivaldosantos <canalandows@gmail.com>
2021-07-16 11:53:02 -0300

    imgs and icons ajustment

--- script.js
+++ script.js
@@ -18,6 +18,24 @@ $(document).ready(function () {
 
   /* OWL CAROUSEL */
   $('.carousel').owlCarousel({
-      
+      margin: 20,
+      loop: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive:{
+          0:{
+              items:1,
+              nav: false
+          },
+          600:{
+            items:2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+      }
+
   })
 });
