30fde4d6287b3eab4d3009b4c7ae2865af58538a
gerssivaldosantos <canalandows@gmail.com>
2021-07-16 09:07:15 -0300

    projects section

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 80){
-            $('.navbar').addClass("sticky")
-        }
-        else{
-            $('.navbar').removeClass("sticky")
-        }
-    });
-    /* Toggle Menu/ NavBar Script */
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass('active')
-        $('.menu-btn i').toggleClass('active')
-    })
-    $('.navbar .menu li a').click(function(){
-        $('.navbar .menu').toggleClass('active')
-        $('.menu-btn i').toggleClass('active')
-    })
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 80) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
+  /* Toggle Menu/ NavBar Script */
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+  $(".navbar .menu li a").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 
-})
+  /* OWL CAROUSEL */
+  $('.carousel').owlCarousel({
+      
+  })
+});
