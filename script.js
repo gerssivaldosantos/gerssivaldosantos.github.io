da967f50d028336c5615f4f77fc187cdbbbbb4d7
gerssivaldosantos <canalandows@gmail.com>
2021-07-17 20:53:22 -0300

    animation complete

--- script.js
+++ script.js
@@ -5,6 +5,30 @@ $(document).ready(function () {
     } else {
       $(".navbar").removeClass("sticky");
     }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+  /* Typing animation */
+  var typed = new Typed(".home-typing", {
+    strings: ["Freelancer", "Developer", "Student", "Tech enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+  });
+
+  var typed = new Typed(".about-typing", {
+    strings: ["Mobile Developer", "Web Developer", "Automation Maker"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+  });
+
+  /* slide-up */
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
   });
   /* Toggle Menu/ NavBar Script */
   $(".menu-btn").click(function () {
@@ -17,25 +41,24 @@ $(document).ready(function () {
   });
 
   /* OWL CAROUSEL */
-  $('.carousel').owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplayTimeOut: 1200,
-      autoplayHoverPause: true,
-      responsive:{
-          0:{
-              items:1,
-              nav: false
-          },
-          600:{
-            items:2,
-            nav: false
-        },
-        1000:{
-            items: 3,
-            nav: false
-        },
-      }
-
-  })
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplayTimeOut: 1200,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
